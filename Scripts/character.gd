f4928228cdd2e0e03d8c183ca72c8e5328175d53
Colter Garrison <colter.garrison@gmail.com>
2024-03-06 12:11:58 -0700

    add dust and sound effects to player

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 
 @export_category("jump")
 @export var _air_control: float = 0.5
+@export var _jump_dust : PackedScene
 @export var _jump_height: float = 2.5
 var _jump_velocity : float
 
@@ -38,6 +39,7 @@ func run(direction : float):
 func jump():
 	if is_on_floor():
 		velocity.y = _jump_velocity
+		_spawn_dust(_jump_dust)
 
 func stop_jump():
 	if velocity.y < 0:
@@ -72,3 +74,9 @@ func _air_physics(delta: float) -> void:
 	velocity.y += gravity * delta
 	if _direction:
 		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _air_control * delta)
+
+func _spawn_dust(dust: PackedScene):
+	var _dust = dust.instantiate()
+	_dust.position = position
+	_dust.flip_h = _sprite.flip_h
+	get_parent().add_child(_dust)
